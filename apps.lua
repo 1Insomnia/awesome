a1f3e92a51327aa51481812f6304c87ccf637a39
r0b0tx <jeremy.pro.lp@gmail.com>
2020-09-29 21:53:53 +0200

    awesome light candy icons init

---
+++ apps.lua
@@ -0,0 +1,58 @@
+--       █████╗ ██████╗ ██████╗ ███████╗
+--      ██╔══██╗██╔══██╗██╔══██╗██╔════╝
+--      ███████║██████╔╝██████╔╝███████╗
+--      ██╔══██║██╔═══╝ ██╔═══╝ ╚════██║
+--      ██║  ██║██║     ██║     ███████║
+--      ╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝
+
+
+-- ===================================================================
+-- Initialization
+-- ===================================================================
+
+
+local awful = require("awful")
+local filesystem = require("gears.filesystem")
+
+-- define module table
+local apps = {}
+
+
+-- ===================================================================
+-- App Declarations
+-- ===================================================================
+
+
+-- define default apps
+apps.default = {
+   terminal = "kitty",
+   launcher = "rofi -matching fuzzy -show combi",
+   lock = "i3lock",
+   screenshot = "scrot -e 'mv $f ~/Pictures/ 2>/dev/null'",
+   filebrowser = "thunar"
+}
+
+-- List of apps to start once on start-up
+local run_on_start_up = {
+  "xrandr --output HDMI1 --auto --right-of eDP1" 
+}
+
+
+-- ===================================================================
+-- Functionality
+-- ===================================================================
+
+
+-- Run all the apps listed in run_on_start_up
+function apps.autostart()
+   for _, app in ipairs(run_on_start_up) do
+      local findme = app
+      local firstspace = app:find(" ")
+      if firstspace then
+         findme = app:sub(0, firstspace - 1)
+      end
+         awful.spawn.with_shell(string.format("pgrep -u $USER -x %s > /dev/null || (%s)", findme, app), false)
+   end
+end
+
+return apps
